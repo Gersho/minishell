ccbada6cb92b0235ed0101eaf49799ba56504be0
hikki <johan.chevet@hotmail.fr>
2021-10-13 13:31:33 +0200

    removed libft functions

--- srcs/s_cmd.c
+++ srcs/s_cmd.c
@@ -12,55 +12,6 @@
 
 #include "../headers/token.h"
 
-void	*ft_memset(void *b, int c, size_t len)
-{
-	size_t			i;
-	unsigned char	*str;
-
-	str = (unsigned char *)b;
-	i = 0;
-	while (i < len)
-	{
-		str[i] = (unsigned char)c;
-		i++;
-	}
-	return (b);
-}
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void	*ptr;
-
-	ptr = malloc(count * size);
-	if (!ptr)
-		return (NULL);
-	ft_memset(ptr, 0, count * size);
-	return (ptr);
-}
-
-size_t	ft_strlen(const char *str)
-{
-	int	i;
-
-	if (!str)
-		return (0);
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-//char	*ft_strdup(const char *s1)
-//{
-//	int		len;
-//	char	*ptr;
-//
-//	len = ft_strlen(s1);
-//	ptr = ft_calloc(len + 1, sizeof(char));
-//	ft_strlcpy(ptr, s1, len + 1);
-//	return (ptr);
-//}
-
 t_cmd	*ft_cmd_init(void)
 {
 	t_cmd	*cmd;
