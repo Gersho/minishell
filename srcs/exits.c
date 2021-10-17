1c9ad73897bd8b7043cef726773c5b633107f040
Zennoune Karim <kzennoun@student.42lyon.fr>
2021-10-17 14:03:32 +0200

    working on free

--- srcs/exits.c
+++ srcs/exits.c
@@ -6,11 +6,38 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 14:12:59 by kzennoun          #+#    #+#             */
-/*   Updated: 2021/10/12 14:51:47 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2021/10/17 14:03:09 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
+
+void	ft_free_cmd(t_cmd *cmd)
+{
+	if (cmd->red)
+		free(cmd->red);
+	
+}
+
+void	ft_cmd_loop(t_cmd *cmd)
+{
+	t_cmd	*tmp;
+
+	while(cmd);
+	{
+		tmp = cmd;
+		while (tmp)
+		{
+			if (tmp->next != NULL)
+				tmp = tmp->next;
+			else
+			{
+				ft_free_cmd(tmp);
+				break ;
+			}
+		}
+	}
+}
 
 void	ft_error_exit(int err)
 {
