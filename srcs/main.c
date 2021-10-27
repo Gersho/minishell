096a7479f9d67759072c8a8327548872a3b1d352
Gersho <zennoune.karim@outlook.fr>
2021-10-28 01:39:17 +0200

    resetting main.c

--- srcs/main.c
+++ srcs/main.c
@@ -6,44 +6,51 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 13:17:49 by kzennoun          #+#    #+#             */
-/*   Updated: 2021/10/26 14:20:11 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2021/10/28 01:38:17 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../headers/minishell.h"
-
-int main()
+//#include <readline/history.h>
+//TODO printf_fd
+int main(int ac,char **av, char** env)
 {
 	char*	line;
+	char	**env_t;
 	t_cmd*	cmd;
+	char 	*prompt;
+	//t_cmd*	tmp;
+
+	(void)ac;
+	(void)av;
+
 	t_cmd*	tmp;
+	t_env	*env_l;
 
-	line = readline("$");
-	//exit(0);
-	cmd = ft_cmd_init();
-	ft_parse_line(line, cmd);
-
-	int i;
-	int j = 0;
-	tmp = cmd;
-
-	while (tmp)
+	env_l = get_env_list(env);
+//	print_list(env_l);
+//	printf("%s\n", set_prompt());
+	//TODO segf if red alone ex: "echo >"
+	while (1)
 	{
-		// printf("maillon cmd: %d", j);
-		// j++;
-		i = 0;
-		printf("#########\n");
-		printf("reds: %s\n", tmp->red);
-		while (tmp->param[i])
+		prompt = set_prompt(env_l);
+		line = readline(prompt);
+//		printf("in main :%s\n", env_l->name);
+		cmd = ft_cmd_init();
+		ft_parse_line(line, cmd);
+//		dprintf(2, "line = |%s| cmd = |%s|\n", line, *cmd->param);
+		if (*cmd->param)
+			exec_cmd(cmd, &env_l);
+		while (cmd != NULL)
 		{
-			printf("----\n");	
-			printf("j: %d | i: %d\n", j, i);
-			//printf("%p\n", tmp->param);
-			printf("cmd param: %s\n", tmp->param[i]);
-			i++;
+			tmp = cmd->next;
+			free(cmd);
+			cmd = NULL;
+			cmd = tmp;
 		}
-		j++;
-		tmp = tmp->next;
+		free(prompt);
+		free(line);
+		line = NULL;
 	}
-	exit(0);
+	return 0;
 }
