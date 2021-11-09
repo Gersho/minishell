f3163792ee50a8f4122ca6fae9d086ad03c7cc33
Zennoune Karim <kzennoun@student.42lyon.fr>
2021-11-09 13:31:34 +0100

    parse toos fix

--- srcs/parsing/parse_tools.c
+++ srcs/parsing/parse_tools.c
@@ -6,7 +6,7 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 17:09:37 by kzennoun          #+#    #+#             */
-/*   Updated: 2021/11/06 15:35:21 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2021/11/09 12:56:53 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 static void	ft_param_loop(t_vars *vars, int *i)
 {
-	while (vars->str[*i])
+	int	len;
+
+	len = ft_strlen(vars->str);
+	//while (vars->str[*i])
+	while (*i < len)
 	{
 		if (vars->str[*i] == ' ')
 			break ;
