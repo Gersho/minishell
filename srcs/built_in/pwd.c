960ea52d4ada4e3b99595a4f535c73b0ef0a1e10
Zennoune Karim <kzennoun@student.42lyon.fr>
2021-11-25 12:43:45 +0100

    pwd fix

--- srcs/built_in/pwd.c
+++ srcs/built_in/pwd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jchevet <jchevet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 08:40:50 by jchevet           #+#    #+#             */
-/*   Updated: 2021/11/09 08:40:50 by jchevet          ###   ########lyon.fr   */
+/*   Updated: 2021/11/25 12:43:28 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,6 @@ int	pwd(char **param)
 {
 	char	buf[PATH_MAX];
 
-	if (param[1] != NULL)
-	{
-		ft_printf_fd(2, "%s: pwd: too many arguments\n", PROMPTERR);
-		return (EXIT_FAILURE);
-	}
-	else
-		ft_printf_fd(1, "%s\n", getcwd(buf, PATH_MAX));
+	ft_printf_fd(1, "%s\n", getcwd(buf, PATH_MAX));
 	return (EXIT_SUCCESS);
 }
