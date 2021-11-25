209d4c802cee8ca43100bcbccbbec2daa3921417
hikki <johan.chevet@hotmail.fr>
2021-11-25 13:28:36 +0100

    d

--- srcs/built_in/pwd.c
+++ srcs/built_in/pwd.c
@@ -16,12 +16,6 @@ int	pwd(char **param)
 {
 	char	buf[PATH_MAX];
 
-	if (param[1] != NULL)
-	{
-		ft_printf_fd(2, "%s: pwd: too many arguments\n", PROMPTERR);
-		return (EXIT_FAILURE);
-	}
-	else
-		ft_printf_fd(1, "%s\n", getcwd(buf, PATH_MAX));
+	ft_printf_fd(1, "%s\n", getcwd(buf, PATH_MAX));
 	return (EXIT_SUCCESS);
 }
